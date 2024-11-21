7b214369588f6825ce12561272e28b66ab3daba7
gayoung lee <ken604@naver.com>
2024-11-21 18:10:49 +0900

    옆에 있는 뱃지 완료

--- js/main.js
+++ js/main.js
@@ -14,3 +14,24 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+      // gsap.to(요소, 지속시간, 옵션);
+      //배지 숨기기
+    } else {
+      gsap.to(badgeEl, 0.6, { opacity: 1, display: "block" });
+      //배지 보이기
+    }
+  }, 300)
+);
+// _.throttle(함수, 시간)
