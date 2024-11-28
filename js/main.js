74d5eb1d6989d50f34514c9bbc7f77dc092fa2b2
gayoung lee <ken604@naver.com>
2024-11-28 20:30:46 +0900

    중간 업데이트

--- js/main.js
+++ js/main.js
@@ -57,4 +57,15 @@ new Swiper(".promotion .swiper-container", {
   slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
   spaceBetween: 10, // 슬라이드 사이 여백
   centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  // autoplay: {
+  //   delay: 5000,
+  // },
+  pagination: {
+    el: ".promotion .swiper-pagination", //페이지 번호 요소 선택자
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
 });
