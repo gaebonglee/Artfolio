6a3b50737dccc9bef8a8ebab5192e612b96161ee
gayoung lee <ken604@naver.com>
2024-11-28 17:24:22 +0900

    차이점확인예정

--- js/main.js
+++ js/main.js
@@ -47,6 +47,23 @@ fadeEls.forEach(function (fadeEl, index) {
 // new Swiper(선택자, 옵션)
 new Swiper(".notice-line .swiper-container", {
   direction: "vertical",
-  // autoplay: true,
-  // loop: true,
+  autoplay: true,
+  loop: true,
+});
+
+new Swiper(".promotion .swiper-container", {
+  loop: true,
+  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  pagination: {
+    // 페이지 번호 사용 여부
+    el: ".promotion .swiper-pagination", // 페이지 번호 요소 선택자
+    clickable: true, // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    // 슬라이드 이전/다음 버튼 사용 여부
+    prevEl: ".promotion .swiper-prev", // 이전 버튼 선택자
+    nextEl: ".promotion .swiper-next", // 다음 버튼 선택자
+  },
 });
