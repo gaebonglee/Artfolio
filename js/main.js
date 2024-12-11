65dfa653aece473558c5716c4d7eec0e03f19e79
gayoung lee <ken604@naver.com>
2024-12-11 23:31:32 +0900

    스크롤에 따라 클래스 생성여부 등록

--- js/main.js
+++ js/main.js
@@ -57,9 +57,9 @@ new Swiper(".promotion .swiper-container", {
   slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
   spaceBetween: 10, // 슬라이드 사이 여백
   centeredSlides: true, // 1번 슬라이드가 가운데 보이기
-  // autoplay: {
-  //   delay: 5000,
-  // },
+  autoplay: {
+    delay: 5000,
+  },
   pagination: {
     el: ".promotion .swiper-pagination", //페이지 번호 요소 선택자
     clickable: true,
@@ -88,7 +88,12 @@ floatingObject(".floating1", 1, 15);
 floatingObject(".floating2", 0.5, 15);
 floatingObject(".floating3", 1.5, 20);
 
-const spyEls = document.querySelector("section.scroll-spy");
+const spyEls = document.querySelectorAll("section.scroll-spy");
 spyEls.forEach(function (spyEl) {
-  new ScrollMagic.Scene().setClassToggle().addTo();
+  new ScrollMagic.Scene({
+    triggerElement: spyEl, //보여짐 여부를 감시할 요소를 지정
+    triggerHook: 0.8, //
+  })
+    .setClassToggle(spyEl, "show")
+    .addTo(new ScrollMagic.Controller());
 });
